7212e594580ade56fe3df13ee170933f7fd40dc3
Eduardo Faria <z.eduardofaria@gmail.com>
2023-04-20 21:12:18 -0300

    add optional parameters

--- enum/html-parser.sh
+++ enum/html-parser.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 
+if [ "$1" = "--help" ]; then
+  echo "Usage: $0 [domain] [--only-address] [--only-ip]"
+  echo "  --only-address: display only the list of new addresses found"
+  echo "  --only-ip: display only a list of IP addresses of the domains found"
+  echo "Exiting."
+  exit 0
+fi
+
 if [ -z "$1" ]; then
-  echo "Uso: $0 [dominio]"
+  echo "Usage: $0 [dominio]"
   exit 1
 fi
 
 domain="$1"
 
 echo "==============================================================================="
-echo "                                    [+] Resolvendo URLs em: [$domain]           "
+echo "                                    [+] URLs in: [$domain]"
 echo "==============================================================================="
-printf "%-6s%-50s%s\n" "Line" "Novo Endereço" "IP"
-echo "--------------------------------------------------------------------------------"
+printf "%-6s%-50s%s\n" "Line" "Address" "IP"
+echo "==============================================================================="
 
 count=0
-wget -q -O - "$domain" | grep -Eoi '<a [^>]+>' | \
-grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^/"]+' | \
+urls=""
+ips=""
+
 while read line; do
   ip=$(host "$(echo "$line" | cut -c8-)" | awk '/has address/ { print $4 }')
   printf "%-6s%-50s%-15s\n" "$count" "$line" "$ip"
+  urls+="$line\n"
+  ips+="$ip\n"
   ((count++))
-done
+done < <(wget -q -O - "$domain" | grep -Eoi '<a [^>]+>' | \
+grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^/"]+')
+echo "==============================================================================="
+
+if [ "$2" = "--only-address" ]; then
+  echo "==============================================================================="
+  echo "                                    [+] Listing only new addresses"
+  echo "==============================================================================="
+  echo -e "$urls"
+  exit 0
+fi
+
+if [ "$2" = "--only-ip" ]; then
+  echo "==============================================================================="
+  echo "                                    [+] Listing only IPs"
+  echo "==============================================================================="
+  echo -e "$ips"
+  exit 0
+fi
